1ebf1036faf0f1ecd4460b4488ae9ab54163fab9
albvs <anton.hlinisty@gmail.com>
2016-10-09 12:42:02 +0300

    Updated ImageTagLib unit tests and UnitTestUtils.

--- src/test/groovy/com/tempvs/taglibs/ImageTagLibSpec.groovy
+++ src/test/groovy/com/tempvs/taglibs/ImageTagLibSpec.groovy
@@ -1,5 +1,9 @@
 package com.tempvs.taglibs
 
+import com.tempvs.domain.user.User
+import com.tempvs.domain.user.UserProfile
+import com.tempvs.tests.unit.UnitTestUtils
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,16 +11,25 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
  */
 @TestFor(ImageTagLib)
+@Mock([User, UserProfile])
 class ImageTagLibSpec extends Specification {
-
     def setup() {
+        tagLib.springSecurityService = [currentUser: UnitTestUtils.createUser()]
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "if user has no avatar - the default one is shown"() {
+        expect: "default avatar is shown"
+        tagLib.userPic().toString().contains '/assets/defaultAvatar.jpg'
+    }
+
+    void "if user has avatar - it is queried"() {
+        given: "assigning avatar to user"
+        tagLib.springSecurityService.currentUser.userProfile.avatar.pathToFile = 'some_file_path'
+
+        expect: "avatar is queried"
+        tagLib.userPic().toString().contains '/userProfile/getAvatar'
     }
 }
--- src/test/groovy/com/tempvs/tests/unit/UnitTestUtils.groovy
+++ src/test/groovy/com/tempvs/tests/unit/UnitTestUtils.groovy
@@ -5,8 +5,9 @@ import com.tempvs.domain.user.User
 import com.tempvs.domain.user.UserProfile
 
 class UnitTestUtils {
-    public static void createUser(String email, String password, String firstName = 'firstName', String lastName = 'lastName',
-                                  String customId = null, String profileEmail = null, String location = null){
+    static User createUser(String email = 'test@mail.com',String password = 'passW0rd!', String firstName = 'firstName',
+                           String lastName = 'lastName', String customId = null, String profileEmail = null,
+                           String location = null){
         User user = new User(email: email, password: password, lastActive: new Date())
         user.userProfile = new UserProfile(firstName:firstName, lastName: lastName, customId:customId,
                 profileEmail: profileEmail, location:location, avatar: new Avatar())
