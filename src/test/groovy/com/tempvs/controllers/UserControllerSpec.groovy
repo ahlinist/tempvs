09c970902ec6afb0704c7555cffe95ec14ecdf00
albvs <anton.hlinisty@gmail.com>
2016-12-10 20:58:51 +0300

    Added UserController unit test.

---
+++ src/test/groovy/com/tempvs/controllers/UserControllerSpec.groovy
@@ -0,0 +1,26 @@
+package com.tempvs.controllers
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(UserController)
+class UserControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "Render register page"() {
+        when: 'Call register() action'
+        controller.register()
+
+        then: 'No modelAndView objects returned and no redirects happen'
+        controller.modelAndView == null
+        response.redirectedUrl == null
+    }
+}
