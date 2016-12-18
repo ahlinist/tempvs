2d6bb3eadce3504a073a1e1878b8c42464c0f599
albvs <anton.hlinisty@gmail.com>
2016-12-18 12:25:38 +0300

    Updated unit tests.

--- src/test/groovy/com/tempvs/controllers/UserControllerSpec.groovy
+++ src/test/groovy/com/tempvs/controllers/UserControllerSpec.groovy
@@ -1,5 +1,10 @@
 package com.tempvs.controllers
 
+import com.tempvs.domain.user.User
+import com.tempvs.domain.user.UserProfile
+import com.tempvs.tests.unit.UnitTestUtils
+import grails.converters.JSON
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,20 +12,180 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(UserController)
+@Mock([User, UserProfile])
 class UserControllerSpec extends Specification {
+    private static final String EXISTENT_CUSTOM_ID = UnitTestUtils.CUSTOM_ID
+    private static final String NON_EXISTENT_CUSTOM_ID = 'non-existentTestCustomId'
+    private static final String MOCKED_RESPONSE = 'mocked_response'
+    private static final String UPDATED = 'updated'
+    private static final String NO_SUCH_USER_SHOW = 'user.show.noSuchUser.message'
+    private static final String EMAIL_UPDATE_DUPLICATE = 'user.edit.email.duplicate'
+    private static final String SHOW_PAGE_URL = '/user/show'
+    private static final String LOGIN_PAGE_URL = '/auth/login'
+
+    private static User mockedUser
 
     def setup() {
+        controller.springSecurityService = [currentUser: user]
+        controller.userService = [
+                getUser: { id ->
+                    user
+                },
+                updateUserProfile: { obj -> },
+                createEmailVerification: {obj -> }
+        ]
+
+        controller.ajaxResponseService = [composeJsonResponse: { obj, str = null ->
+            [success: MOCKED_RESPONSE] as JSON
+        }]
     }
 
     def cleanup() {
     }
 
-    void "Render register page"() {
-        when: 'Call register() action'
-        controller.register()
+    void "Check show page being not logged in"() {
+        given:
+        controller.springSecurityService = [:]
 
-        then: 'No modelAndView objects returned and no redirects happen'
+        when: 'Calling show() action'
+        controller.show()
+
+        then: 'Being redirected to login page'
+        response.redirectedUrl == LOGIN_PAGE_URL
+    }
+
+    void "Check show page being logged in"() {
+        when: 'Call show() action without id'
+        controller.show()
+
+        then: 'Request is redirected to login page'
+        response.redirectedUrl == "${SHOW_PAGE_URL}/${EXISTENT_CUSTOM_ID}"
+    }
+
+    void "Check show page being not logged in, passing id of non-existent user"() {
+        given:
+        controller.userService = [getUser: { id -> }]
+
+        when: 'Call show() action with id'
+        params.id = NON_EXISTENT_CUSTOM_ID
+        def model = controller.show()
+
+        then: 'Show page is rendered with a warning'
         controller.modelAndView == null
         response.redirectedUrl == null
+        model == [id: NON_EXISTENT_CUSTOM_ID, message: NO_SUCH_USER_SHOW, args: [NON_EXISTENT_CUSTOM_ID]]
+    }
+
+    void "Check show page being not logged in, passing id of existent user"() {
+        given: "Mock user"
+        controller.springSecurityService = [:]
+        controller.userService = [getUser: { id ->
+            user
+        }]
+
+        when: 'Call show() action with id'
+        params.id = EXISTENT_CUSTOM_ID
+        def model = controller.show()
+
+        then: 'Show page is rendered with a warning'
+        controller.modelAndView == null
+        response.redirectedUrl == null
+        model == [id: EXISTENT_CUSTOM_ID, user: user]
+    }
+
+    void "Check show page being logged in, passing own id"() {
+        when: 'Call show() action with id'
+        params.id = EXISTENT_CUSTOM_ID
+        def model = controller.show()
+
+        then: 'Request is redirected to login page'
+        controller.modelAndView == null
+        response.redirectedUrl == null
+        model == [user:user, id: EXISTENT_CUSTOM_ID]
+    }
+
+    void "Render edit user page"() {
+        when: 'Call edit()'
+        def model = controller.edit()
+
+        then: 'Edit page rendered'
+        controller.modelAndView == null
+        response.redirectedUrl == null
+        model == [user:user]
+    }
+
+    void "Update user email with duplicate"() {
+        when: 'Call updateEmail() with param'
+        params.email = UnitTestUtils.EMAIL
+        controller.updateEmail()
+
+        then: 'Warning message returned'
+        response.text == emailUpdateDuplicateJson as String
+    }
+
+    void "Update user email"() {
+        when: 'Call updateEmail() with param'
+        params.email = UPDATED + UnitTestUtils.EMAIL
+        controller.updateEmail()
+
+        then: 'Mocked JSON response returned'
+        response.json.success == MOCKED_RESPONSE
+    }
+
+    void "Update user password incorrectly"() {
+        when: 'Call updatePassword()'
+        controller.updatePassword()
+
+        then: 'Mocked JSON response returned'
+        response.json.success == MOCKED_RESPONSE
+    }
+
+    void "Render profile page"() {
+        when: 'Call profile()'
+        def model = controller.profile()
+
+        then: 'Edit page rendered'
+        controller.modelAndView == null
+        response.redirectedUrl == null
+        model == [user:user]
+    }
+
+    void "Update profile"() {
+        when: 'Call updateUserProfile()'
+        controller.updateUserProfile()
+
+        then: 'Mocked JSON response returned'
+        response.json.success == MOCKED_RESPONSE
+    }
+
+    void "Update profileEmail"() {
+        when: 'Call updateProfileEmail()'
+        controller.updateProfileEmail()
+
+        then: 'Mocked JSON response returned'
+        response.json.success == MOCKED_RESPONSE
+    }
+
+    void "Update avatar"() {
+        given: 'Mock imageService'
+        controller.imageService = [updateAvatar: { obj -> }]
+
+        when: 'Call updateAvatar()'
+        controller.updateAvatar()
+
+        then: 'Mocked JSON response returned'
+        response.json.success == MOCKED_RESPONSE
+    }
+
+    private static JSON getEmailUpdateDuplicateJson() {
+        [messages: [EMAIL_UPDATE_DUPLICATE]] as JSON
+    }
+
+    private static User getUser() {
+        if (!this.mockedUser) {
+            this.mockedUser = UnitTestUtils.createUser()
+        }
+
+        return this.mockedUser
     }
 }
--- src/test/groovy/com/tempvs/tests/unit/UnitTestUtils.groovy
+++ src/test/groovy/com/tempvs/tests/unit/UnitTestUtils.groovy
@@ -5,13 +5,16 @@ import com.tempvs.domain.user.User
 import com.tempvs.domain.user.UserProfile
 
 class UnitTestUtils {
-    public static final String CUSTOM_ID = 'testCustomId'
-    public static final String EMAIL = 'test@mail.com'
-    public static final String PASSWORD = 'passW0rd!'
+    public static final String CUSTOM_ID = 'defaultTestCustomId'
+    public static final String EMAIL = 'defaultTest@mail.com'
+    public static final String PASSWORD = 'defaultPassW0rd!'
+    public static final String FIRST_NAME = 'defaultFirstName'
+    public static final String LAST_NAME = 'defaultLastName'
+    public static final String LOCATION = 'defaultLocation'
 
-    static User createUser(String email = EMAIL,String password = PASSWORD, String firstName = 'firstName',
-                           String lastName = 'lastName', String customId = CUSTOM_ID, String profileEmail = null,
-                           String location = null){
+    static User createUser(String email = EMAIL,String password = PASSWORD, String firstName = FIRST_NAME,
+                           String lastName = LAST_NAME, String customId = CUSTOM_ID, String profileEmail = null,
+                           String location = LOCATION){
         User user = new User(email: email, password: password, lastActive: new Date())
         user.userProfile = new UserProfile(firstName:firstName, lastName: lastName, customId:customId,
                 profileEmail: profileEmail, location:location, avatar: new Avatar())
