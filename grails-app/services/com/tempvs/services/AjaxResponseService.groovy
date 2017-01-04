1d09c2fa208a47973dc7894a0ca8a48c6ad5aca6
albvs <anton.hlinisty@gmail.com>
2017-01-04 20:48:40 +0300

    Refactored Ajax response logic.

--- grails-app/services/com/tempvs/services/AjaxResponseService.groovy
+++ grails-app/services/com/tempvs/services/AjaxResponseService.groovy
@@ -1,14 +1,27 @@
 package com.tempvs.services
 
-import com.tempvs.ajax.AjaxResponse
 import grails.converters.JSON
 import grails.transaction.Transactional
+import org.springframework.context.i18n.LocaleContextHolder
 
 @Transactional
 class AjaxResponseService {
-    def ajaxResponseFactory
+    def messageSource
 
     JSON composeJsonResponse(instance, String successMessage = null) {
-        ajaxResponseFactory.newInstance(instance, successMessage) as JSON
+        Boolean success = Boolean.TRUE
+        Set messages = []
+
+        if (instance.hasErrors()) {
+            success = Boolean.FALSE
+
+            instance.errors.allErrors.each {
+                messages << messageSource.getMessage(it, LocaleContextHolder.locale)
+            }
+        } else {
+            messages << messageSource.getMessage(successMessage, null, LocaleContextHolder.locale)
+        }
+
+        [success: success, messages: messages] as JSON
     }
 }
--- src/main/groovy/com/tempvs/ajax/AjaxResponseFactory.groovy
+++
@@ -1,7 +0,0 @@
-package com.tempvs.ajax
-
-class AjaxResponseFactory {
-    AjaxResponse newInstance(instance, String successMessage = null) {
-        new AjaxResponse(instance, successMessage)
-    }
-}
