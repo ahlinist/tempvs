af23e0b5a816479490783857dc46a01e8efa367f
ahlinist <anton.hlinisty@gmail.com>
2017-03-09 20:50:32 +0300

    Updated AuthController unit tests.

--- src/test/groovy/com/tempvs/controllers/AuthControllerSpec.groovy
+++ src/test/groovy/com/tempvs/controllers/AuthControllerSpec.groovy
@@ -2,49 +2,50 @@ package com.tempvs.controllers
 
 import com.tempvs.domain.user.User
 import com.tempvs.domain.user.UserProfile
-import com.tempvs.tests.utils.TestingUtils
-import com.tempvs.tests.utils.user.WithUser
+import com.tempvs.domain.user.verification.EmailVerification
+import com.tempvs.services.AjaxResponseService
+import com.tempvs.services.UserService
 import grails.converters.JSON
+import grails.plugin.springsecurity.SpringSecurityService
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
+import org.grails.plugins.testing.GrailsMockHttpServletResponse
+import org.springframework.security.authentication.encoding.PasswordEncoder
 import spock.lang.Specification
-
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(AuthController)
 @Mock([User, UserProfile])
-class AuthControllerSpec extends Specification implements WithUser {
-    private static final String INCORRECT = 'incorrect'
-    private static final String NO_SUCH_USER_LOGIN = 'user.login.noSuchUser.message'
-    private static final String MOCKED_RESPONSE = 'mocked_response'
-    private static final String SHOW_PAGE_URL = '/user/show'
+class AuthControllerSpec extends Specification {
+    private static final String EMAIL = 'authUnitTest@email.com'
+    private static final String PASSWORD = 'password'
+    private static final String REGISTER_ACTION = 'register'
+    private static final String NO_SUCH_USER_MESSAGE = 'auth.login.noSuchUser.message'
+    private static final String SHOW_PAGE_URI = '/user/show'
+
+    def emailVerification = Mock(EmailVerification)
+    def requestRegistrationCommand = Mock(RequestRegistrationCommand)
+    def ajaxResponseService = Mock(AjaxResponseService)
+    def userService = Mock(UserService)
+    def json = Mock(JSON)
+    def passwordEncoder = Mock(PasswordEncoder)
+    def springSecurityService = Mock(SpringSecurityService)
+    def loginCommand = Mock(LoginCommand)
+    def user = Mock(User)
 
     def setup() {
-        controller.ajaxResponseService = [composeJsonResponse: { obj, string = null ->
-            [success: MOCKED_RESPONSE] as JSON
-        }]
-
-        controller.userService = [
-                getUserByEmail:{ email ->
-                    if (email == TestingUtils.EMAIL) {
-                        return user
-                    }
-                }
-        ]
-
-        controller.passwordEncoder = [isPasswordValid: { userPass, inputPass, arg ->
-            if (userPass == inputPass) {
-                true
-            }
-        }]
+        controller.ajaxResponseService = ajaxResponseService
+        controller.userService = userService
+        controller.passwordEncoder = passwordEncoder
+        controller.springSecurityService = springSecurityService
     }
 
     def cleanup() {
     }
 
     void "Render register page"() {
-        when: 'Call register() action'
+        when: 'Register() action is called'
         controller.register()
 
         then: 'No modelAndView objects returned and no redirects happen'
@@ -52,13 +53,30 @@ class AuthControllerSpec extends Specification implements WithUser {
         response.redirectedUrl == null
     }
 
-    void "Passing params into register form"() {
-        when: 'Passing register params'
+    void "Testing register() action with invalid params"() {
+        when: 'Passing command mock'
         params.isAjaxRequest = Boolean.TRUE
-        controller.register()
+        controller.register(requestRegistrationCommand)
 
         then: 'JSON response returned'
-        response.json.success == MOCKED_RESPONSE
+        1 * requestRegistrationCommand.validate() >> Boolean.FALSE
+        1 * ajaxResponseService.composeJsonResponse(requestRegistrationCommand) >> json
+        1 * json.render(_ as GrailsMockHttpServletResponse)
+        0 * _
+    }
+
+    void "Testing register() action with valid params"() {
+        when: 'Passing command mock'
+        params.isAjaxRequest = Boolean.TRUE
+        controller.register(requestRegistrationCommand)
+
+        then: 'JSON response returned'
+        1 * requestRegistrationCommand.validate() >> Boolean.TRUE
+        1 * requestRegistrationCommand.getEmail() >> EMAIL
+        1 * userService.createEmailVerification(['action': REGISTER_ACTION, 'email': EMAIL]) >> emailVerification
+        1 * ajaxResponseService.composeJsonResponse(emailVerification, _ as String) >> json
+        1 * json.render(_ as GrailsMockHttpServletResponse)
+        0 * _
     }
 
     void "Render login page"() {
@@ -70,63 +88,61 @@ class AuthControllerSpec extends Specification implements WithUser {
         response.redirectedUrl == null
     }
 
-    void "Pass invalid params into login form"() {
-        when: 'Passing non-valid params'
+    void "Testing login() for non-valid params"() {
+        when: 'Passing command mock'
         params.isAjaxRequest = Boolean.TRUE
-        params.email = TestingUtils.EMAIL
-        controller.login()
+        controller.login(loginCommand)
 
-        then: 'Mocked JSON response returned'
-        response.json.success == MOCKED_RESPONSE
+        then: 'JSON response returned'
+        1 * loginCommand.validate() >> Boolean.FALSE
+        1 * ajaxResponseService.composeJsonResponse(loginCommand) >> json
+        1 * json.render(_ as GrailsMockHttpServletResponse)
+        0 * _
     }
 
-    void "Pass valid params but with incorrect email into login form"() {
-        when: 'Passing email for existent user'
+    void "Testing login() for non-existing user"() {
+        when: 'Passing command mock'
         params.isAjaxRequest = Boolean.TRUE
-        params.email = INCORRECT + TestingUtils.EMAIL
-        params.password = TestingUtils.PASSWORD
-        controller.login()
+        controller.login(loginCommand)
 
-        then: 'JSON response with the corresponding message returned'
-        controller.modelAndView == null
-        response.redirectedUrl == null
-        response.text == noSuchUserJson as String
+        then: 'JSON response returned'
+        1 * loginCommand.validate() >> Boolean.TRUE
+        1 * loginCommand.getEmail() >> EMAIL
+        1 * userService.getUserByEmail(EMAIL) >> null
+        0 * _
+        response.json.messages == [NO_SUCH_USER_MESSAGE]
     }
 
-    void "Pass valid params but with incorrect pass into login form"() {
-        when: 'Passing email for existent user'
+    void "Testing login() for incorrect password"() {
+        when: 'Passing command mock'
         params.isAjaxRequest = Boolean.TRUE
-        params.email = TestingUtils.EMAIL
-        params.password = INCORRECT + TestingUtils.PASSWORD
-        controller.login()
+        controller.login(loginCommand)
 
-        then: 'JSON response with the corresponding message returned'
-        controller.modelAndView == null
-        response.redirectedUrl == null
-        response.text == noSuchUserJson as String
+        then: 'JSON response returned'
+        1 * loginCommand.validate() >> Boolean.TRUE
+        1 * loginCommand.getEmail() >> EMAIL
+        1 * userService.getUserByEmail(EMAIL) >> user
+        1 * user.getProperty(PASSWORD) >> PASSWORD
+        1 * loginCommand.getPassword() >> PASSWORD
+        1 * passwordEncoder.isPasswordValid(PASSWORD, PASSWORD, null) >> Boolean.FALSE
+        0 * _
+        response.json.messages == [NO_SUCH_USER_MESSAGE]
     }
 
-    void "Pass valid and correct params into login form"() {
-        given: 'Mocking springSecurityService'
-        controller.springSecurityService = [reauthenticate:{arg1, arg2 -> }]
-
-        when: 'Passing email for existent user'
+    void "Testing login() for correct params"() {
+        when: 'Passing command mock'
         params.isAjaxRequest = Boolean.TRUE
-        params.email = TestingUtils.EMAIL
-        params.password = TestingUtils.PASSWORD
-        controller.login()
+        controller.login(loginCommand)
 
-        then: 'JSON response with show page redirect returned'
-        controller.modelAndView == null
-        response.redirectedUrl == null
-        response.text == showPageRedirectJson as String
-    }
-
-    private static JSON getShowPageRedirectJson() {
-        [redirect: SHOW_PAGE_URL] as JSON
-    }
-
-    private static JSON getNoSuchUserJson() {
-        [messages: [NO_SUCH_USER_LOGIN]] as JSON
+        then: 'JSON response returned'
+        1 * loginCommand.validate() >> Boolean.TRUE
+        2 * loginCommand.getEmail() >> EMAIL
+        1 * userService.getUserByEmail(EMAIL) >> user
+        1 * user.getProperty(PASSWORD) >> PASSWORD
+        2 * loginCommand.getPassword() >> PASSWORD
+        1 * passwordEncoder.isPasswordValid(PASSWORD, PASSWORD, null) >> Boolean.TRUE
+        1 * springSecurityService.reauthenticate(EMAIL, PASSWORD)
+        0 * _
+        response.json.redirect == SHOW_PAGE_URI
     }
 }
