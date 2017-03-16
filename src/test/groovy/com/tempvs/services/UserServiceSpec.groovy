2328c025f7e7f9353db90a133a15b7c0ac47d50b
ahlinist <anton.hlinisty@gmail.com>
2017-03-17 00:21:56 +0300

    Updated UserService unit tests.

--- src/test/groovy/com/tempvs/services/UserServiceSpec.groovy
+++ src/test/groovy/com/tempvs/services/UserServiceSpec.groovy
@@ -3,149 +3,218 @@ package com.tempvs.services
 import com.tempvs.domain.user.User
 import com.tempvs.domain.user.UserProfile
 import com.tempvs.domain.user.verification.EmailVerification
-import com.tempvs.tests.utils.TestingUtils
-import com.tempvs.tests.utils.user.WithUser
-import grails.test.mixin.Mock
+import grails.plugin.springsecurity.SpringSecurityService
+import grails.plugins.mail.MailService
 import grails.test.mixin.TestFor
+import org.codehaus.groovy.runtime.InvokerHelper
 import spock.lang.Specification
-
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 
 @TestFor(UserService)
-@Mock([User, UserProfile, EmailVerification])
-class UserServiceSpec extends Specification implements WithUser {
-    private static final String NEW_EMAIL = 'newEmail@mail.com'
-    private static final String NEW_PROFILE_EMAIL = 'newProfileEmail@mail.com'
-    private static final String NEW_PASSWORD = 'newPassword'
+class UserServiceSpec extends Specification {
+    private static final String ID = '1'
+    private static final Long LONG_ID = 1L
+    private static final String CUSTOM_ID = 'customId'
+    private static final String USER = 'user'
+    private static final String EMAIL = 'email'
+    private static final String PROFILE_EMAIL = 'profileEmail'
+    private static final String PASSWORD = 'password'
     private static final String VERIFICATION_CODE = 'verificationCode'
+    private static final String USER_PROFILE = 'userProfile'
+    private static final String LAST_ACTIVE = 'lastActive'
+
+    def springSecurityService = Mock(SpringSecurityService)
+    def mailService = Mock(MailService)
+    def user = Mock(User)
+    def userProfile = Mock(UserProfile)
+    def emailVerification = Mock(EmailVerification)
 
     def setup() {
         GroovySpy(User, global: true)
         GroovySpy(UserProfile, global: true)
         GroovySpy(EmailVerification, global: true)
+        GroovySpy(InvokerHelper, global: true)
 
-        service.mailService = [sendMail: { Closure c-> }]
-
-        service.springSecurityService = [
-                encodePassword: { arg ->
-                    arg
-                }
-        ]
+        service.mailService = mailService
+        service.springSecurityService = springSecurityService
     }
 
     def cleanup() {
     }
 
     void "Check getUser() method for id"() {
-        when: 'getUser() is called'
-        service.getUser(TestingUtils.USER_ID as String)
+        when:
+        def result = service.getUser(ID)
 
-        then: 'User is queried in the DB'
-        1 * User.get(TestingUtils.USER_ID) >> Mock(User)
+        then:
+        1 * UserProfile.findByCustomId(ID) >> null
+        1 * User.get(_) >> user
+        0 * _
+
+        and:
+        result == user
     }
 
     void "Check getUser() method for customId"() {
-        when: 'getUser() is called'
-        service.getUser(TestingUtils.CUSTOM_ID)
+        when:
+        def result = service.getUser(CUSTOM_ID)
 
-        then: 'User is queried in the DB'
-        1 * UserProfile.findByCustomId(TestingUtils.CUSTOM_ID) >> Mock(User)
+        then:
+        1 * UserProfile.findByCustomId(CUSTOM_ID) >> userProfile
+        1 * userProfile.getProperty(USER) >> user
+
+        and:
+        result == user
     }
 
     void "Check getUserByEmail()"() {
-        when: 'getUserByEmail() is called'
-        service.getUserByEmail(TestingUtils.EMAIL)
+        when:
+        def result = service.getUserByEmail(EMAIL)
 
-        then: 'User is queried in the DB'
-        1 * User.findByEmail(TestingUtils.EMAIL) >> Mock(User)
+        then:
+        1 * User.findByEmail(EMAIL) >> user
+
+        and:
+        result == user
     }
 
     void "Check getUserByProfileEmail()"() {
-        when: 'getUserByProfileEmail() is called'
-        service.getUserByProfileEmail(TestingUtils.PROFILE_EMAIL)
+        when:
+        def result = service.getUserByProfileEmail(PROFILE_EMAIL)
 
-        then: 'User is queried in the DB'
-        1 * UserProfile.findByProfileEmail(TestingUtils.PROFILE_EMAIL) >> Mock(User)
-    }
+        then:
+        1 * UserProfile.findByProfileEmail(PROFILE_EMAIL) >> userProfile
+        1 * userProfile.getProperty(USER) >> user
 
-    void "Check creation of email verification"() {
-        when: 'Calling createEmailVerification()'
-        def result = service.createEmailVerification(TestingUtils.DEFAULT_EMAIL_VERIFICATION_PROPS)
-
-        then: 'Verification constructor invoked and code generated'
-        1 * new EmailVerification(_)
-        result.verificationCode
-    }
-
-    void "Check user creation"() {
-        when: 'Invoking createUser()'
-        def result = service.createUser(TestingUtils.DEFAULT_USER_PROPS)
-
-        then: "Appropriate constructors are called"
-        1 * new User(_)
-        1 * new UserProfile(_)
-
-        and: 'User is returned'
-        result instanceof User
-    }
-
-    void "Check email update"() {
-        expect: "updateEmail() changes user's email"
-        service.updateEmail(user.id, NEW_EMAIL).email == NEW_EMAIL
-
-        and: 'User with updated email is found in the DB'
-        User.findByEmail(NEW_EMAIL)
-    }
-
-    void "Check password update"() {
-        setup: 'Setting up currentUser'
-        service.springSecurityService.currentUser = getUser()
-
-        expect: "updatePassword() changes user's password"
-        service.updatePassword(NEW_PASSWORD).password == NEW_PASSWORD
-
-        and: 'User with updated password is found in the DB'
-        User.findByPassword(NEW_PASSWORD)
-    }
-
-    void "Check updateLastActive()"() {
-        given: 'Initial settings'
-        service.springSecurityService.currentUser = user
-        Date lastActiveOld = user.lastActive
-
-        when: 'Invoking updateLastActive()'
-        service.updateLastActive()
-
-        then: 'Old active date differs from new one'
-        lastActiveOld != user.lastActive
-    }
-
-    void "Check profile email update"() {
-        expect: "updateProfileEmail() changes user's email"
-        service.updateProfileEmail(user.id, NEW_PROFILE_EMAIL).profileEmail == NEW_PROFILE_EMAIL
-
-        and: 'userProfile with updated profileEmail is found in the DB'
-        UserProfile.findByProfileEmail(NEW_PROFILE_EMAIL)
-    }
-
-    void "Check update of user's profile"() {
-        setup: 'Setting up currentUser'
-        service.springSecurityService.currentUser = user
-
-        expect: "updateUserProfile() changes user's password"
-        service.updateUserProfile([location: 'newLocation']).location == 'newLocation'
-
-        and: 'userProfile with updated location is found in the DB'
-        UserProfile.findByLocation('newLocation')
+        and:
+        result == user
     }
 
     void "Check getVerification()"() {
-        when: 'Invoking getVerification()'
-        service.getVerification(VERIFICATION_CODE)
+        when:
+        def result = service.getVerification(VERIFICATION_CODE)
+
+        then:
+        1 * EmailVerification.findByVerificationCode(VERIFICATION_CODE) >> emailVerification
+
+        and:
+        result == emailVerification
+    }
+
+    void "Check successful creation of email verification"() {
+        when:
+        def result = service.createEmailVerification([email: EMAIL])
+
+        then: 'Verification created and mail sent'
+        1 * new EmailVerification(_) >> emailVerification
+        1 * emailVerification.save([flush: true]) >> emailVerification
+        1 * mailService.sendMail(_)
+
+        and:
+        result == emailVerification
+    }
+
+    void "Check failed creation of email verification"() {
+        when:
+        def result = service.createEmailVerification([email: EMAIL])
+
+        then: 'Verification created, not saved and not sent'
+        1 * new EmailVerification(_) >> emailVerification
+        1 * emailVerification.save([flush: true])
+
+        and:
+        result == emailVerification
+    }
+
+    void "Check user creation"() {
+        given:
+        Map properties = [:]
+
+        when:
+        def result = service.createUser(properties)
 
         then: "Appropriate constructors are called"
-        1 * EmailVerification.findByVerificationCode(VERIFICATION_CODE) >> Mock(EmailVerification)
+        1 * new User(properties) >> user
+        1 * user.setProperty(PASSWORD, PASSWORD)
+        1 * user.getProperty(PASSWORD) >> PASSWORD
+        1 * user.getProperty('springSecurityService') >> springSecurityService
+        1 * springSecurityService.encodePassword(_) >> PASSWORD
+        1 * user.setProperty(USER_PROFILE, userProfile)
+        1 * new UserProfile(properties) >> userProfile
+        1 * user.save([flush: true])
+
+        and:
+        result == user
     }
+
+    void "Check email update"() {
+        when:
+        def result = service.updateEmail(LONG_ID, EMAIL)
+
+        then:
+        1 * User.get(LONG_ID) >> user
+        1 * user.setEmail(EMAIL)
+        1 * user.save([flush: true])
+
+        and:
+        result == user
+    }
+
+    void "Check password update"() {
+        when:
+        def result = service.updatePassword(PASSWORD)
+
+        then:
+        1 * springSecurityService.currentUser >> user
+        1 * springSecurityService.encodePassword(PASSWORD) >> PASSWORD
+        1 * user.setPassword(PASSWORD)
+        1 * user.save([flush: true])
+
+        and:
+        result == user
+    }
+
+    void "Check updateLastActive()"() {
+        when:
+        service.updateLastActive()
+
+        then:
+        1 * springSecurityService.currentUser >> user
+        1 * user.setLastActive(_ as Date)
+        1 * user.save([flush: true])
+    }
+
+    void "Check profile email update"() {
+        when:
+        def result = service.updateProfileEmail(LONG_ID, PROFILE_EMAIL)
+
+        then:
+        1 * User.get(LONG_ID) >> user
+        1 * user.getProperty(USER_PROFILE) >> userProfile
+        1 * userProfile.setProfileEmail(PROFILE_EMAIL)
+        1 * userProfile.save([flush: true])
+
+        and:
+        result == userProfile
+    }
+
+    void "Check update of user's profile"() {
+        given:
+        Map properties = [:]
+
+        when:
+        def result = service.updateUserProfile(properties)
+
+        then:
+        1 * springSecurityService.currentUser >> user
+        1 * user.getProperty(USER_PROFILE) >> userProfile
+        1 * InvokerHelper.setProperties(userProfile, properties)
+        1 * userProfile.save([flush: true])
+
+        and:
+        result == userProfile
+    }
+
 }
