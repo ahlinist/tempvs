c0397affb260323fa171bff5da05e026a0f1b2cd
ahlinist <anton.hlinisty@gmail.com>
2017-03-18 23:47:28 +0300

    Updated ImageTagLib unit tests.

--- src/test/groovy/com/tempvs/taglibs/ImageTagLibSpec.groovy
+++ src/test/groovy/com/tempvs/taglibs/ImageTagLibSpec.groovy
@@ -2,38 +2,49 @@ package com.tempvs.taglibs
 
 import com.tempvs.domain.user.User
 import com.tempvs.domain.user.UserProfile
-import com.tempvs.tests.utils.user.WithUser
+import grails.plugin.springsecurity.SpringSecurityService
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
-
 /**
  * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
  */
 @TestFor(ImageTagLib)
 @Mock([User, UserProfile])
-class ImageTagLibSpec extends Specification implements WithUser {
-    private static final String AVATAR_URL = '/user/getAvatar'
+class ImageTagLibSpec extends Specification {
+    private static final String AVATAR_URL = '/image/getAvatar'
+
+    def springSecurityService = Mock(SpringSecurityService)
+    def user = Mock(User)
 
     def setup() {
+        tagLib.springSecurityService = springSecurityService
     }
 
     def cleanup() {
     }
 
     void "If user is not logged in - nothing is returned"() {
-        given: "Setting up the user"
-        tagLib.springSecurityService = [currentUser: null]
+        when:
+        def result = tagLib.userPic()
 
-        expect: "Nothing returned"
-        !tagLib.userPic()
+        then:
+        1 * springSecurityService.currentUser
+        0 * _
+
+        and:
+        !result
     }
 
     void "If user is logged in - userPic is queried"() {
-        given: "Setting up the user"
-        tagLib.springSecurityService = [currentUser: user]
+        when:
+        def result = tagLib.userPic()
 
-        expect: "UserPic is queried"
-        tagLib.userPic().toString().contains AVATAR_URL
+        then:
+        1 * springSecurityService.currentUser >> user
+        0 * _
+
+        and:
+        result.toString().contains AVATAR_URL
     }
 }
