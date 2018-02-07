1c6db816ccfac745b57c2749b1423f69919524c0
ahlinist <anton.hlinisty@gmail.com>
2018-02-08 00:26:43 +0300

    Updated the mongodb configuration.

--- src/main/groovy/com/tempvs/mongodb/GridFSFactory.groovy
+++ src/main/groovy/com/tempvs/mongodb/GridFSFactory.groovy
@@ -9,14 +9,9 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class GridFSFactory {
 
-    private static final String user = System.getenv'MONGO_USER'
-    private static final String password = System.getenv'MONGO_PASS'
-    private static final String host = System.getenv'MONGO_HOST'
-    private static final String port = System.getenv'MONGO_PORT'
-    private static final String databaseName = System.getenv'MONGO_DB_NAME'
-    private static final String uriString = "mongodb://${user}:${password}@${host}:${port}/${databaseName}"
+    private static final String MONGODB_URI = System.getenv 'MONGODB_URI'
 
-    DB mongoDB = new DB(new MongoClient(new MongoClientURI(uriString)), databaseName)
+    DB mongoDB = new DB(new MongoClient(new MongoClientURI(MONGODB_URI)), MONGODB_URI.split('/').last())
 
     GridFS getGridFS(String collection) {
         new GridFS(mongoDB, collection)
