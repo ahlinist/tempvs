d34518008cb62b63532c07dbdc2dafd699b34917
Anton Hlinisty <anton.hlinisty@gmail.com>
2018-11-02 00:55:02 +0300

    #79. Fix unit-tests

--- src/test/groovy/club/tempvs/rest/RestCallerSpec.groovy
+++ src/test/groovy/club/tempvs/rest/RestCallerSpec.groovy
@@ -11,6 +11,8 @@ class RestCallerSpec extends Specification {
 
     private static final String URL = 'url'
     private static final String AUTHORIZATION = 'Authorization'
+    private static final String ACCEPT_LANGUAGE = 'Accept-Language'
+    private static final String ENGLISH = 'en'
     private static final String TEST_JSON = 'test json'
     private static final String CONTENT_TYPE = 'Content-Type'
     private static final String JSON_CONTENT_TYPE = 'application/json; charset=UTF-8'
@@ -33,12 +35,15 @@ class RestCallerSpec extends Specification {
     }
 
     void "Test doGet()"() {
+        given:
+        Map<String, String> headers = [(AUTHORIZATION): AUTHORIZATION, (ACCEPT_LANGUAGE): ENGLISH]
+
         when:
         def result = restCaller.doGet(URL, AUTHORIZATION)
 
         then:
         1 * restHelper.newTemplate() >> restTemplate
-        1 * restHelper.newHttpEntity([(AUTHORIZATION): AUTHORIZATION]) >> httpEntity
+        1 * restHelper.newHttpEntity(headers) >> httpEntity
         1 * restTemplate.exchange(URL, HttpMethod.GET, httpEntity, String.class) >> responseEntity
         1 * restHelper.newRestResponse(responseEntity) >> restResponse
         0 * _
@@ -48,12 +53,15 @@ class RestCallerSpec extends Specification {
     }
 
     void "Test doHead()"() {
+        given:
+        Map<String, String> headers = [(AUTHORIZATION): AUTHORIZATION, (ACCEPT_LANGUAGE): ENGLISH]
+
         when:
         def result = restCaller.doHead(URL, AUTHORIZATION)
 
         then:
         1 * restHelper.newTemplate() >> restTemplate
-        1 * restHelper.newHttpEntity([(AUTHORIZATION): AUTHORIZATION]) >> httpEntity
+        1 * restHelper.newHttpEntity(headers) >> httpEntity
         1 * restTemplate.exchange(URL, HttpMethod.HEAD, httpEntity, String.class) >> responseEntity
         1 * restHelper.newRestResponse(responseEntity) >> restResponse
         0 * _
@@ -63,13 +71,20 @@ class RestCallerSpec extends Specification {
     }
 
     void "Test doPost()"() {
+        given:
+        Map<String, String> headers = [
+                (AUTHORIZATION): AUTHORIZATION,
+                (ACCEPT_LANGUAGE): ENGLISH,
+                (CONTENT_TYPE): JSON_CONTENT_TYPE,
+        ]
+
         when:
         def result = restCaller.doPost(URL, AUTHORIZATION, json)
 
         then:
         1 * restHelper.newTemplate() >> restTemplate
         1 * json.toString() >> TEST_JSON
-        1 * restHelper.newHttpEntity([(AUTHORIZATION): AUTHORIZATION, (CONTENT_TYPE): JSON_CONTENT_TYPE], TEST_JSON) >> httpEntity
+        1 * restHelper.newHttpEntity(headers, TEST_JSON) >> httpEntity
         1 * restTemplate.exchange(URL, HttpMethod.POST, httpEntity, String.class) >> responseEntity
         1 * restHelper.newRestResponse(responseEntity) >> restResponse
         0 * _
@@ -79,12 +94,15 @@ class RestCallerSpec extends Specification {
     }
 
     void "Test doDelete()"() {
+        given:
+        Map<String, String> headers = [(AUTHORIZATION): AUTHORIZATION, (ACCEPT_LANGUAGE): ENGLISH]
+
         when:
         def result = restCaller.doDelete(URL, AUTHORIZATION)
 
         then:
         1 * restHelper.newTemplate() >> restTemplate
-        1 * restHelper.newHttpEntity([(AUTHORIZATION): AUTHORIZATION]) >> httpEntity
+        1 * restHelper.newHttpEntity(headers) >> httpEntity
         1 * restTemplate.exchange(URL, HttpMethod.DELETE, httpEntity, String.class) >> responseEntity
         1 * restHelper.newRestResponse(responseEntity) >> restResponse
         0 * _
