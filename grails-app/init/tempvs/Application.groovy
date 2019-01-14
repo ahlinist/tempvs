98e053f60596a82afb70cd434790988dc647f9cd
Anton Hlinisty <anton.hlinisty@gmail.com>
2019-01-14 19:55:04 +0300

    Remove @EnableDiscoveryClient annotation

--- grails-app/init/tempvs/Application.groovy
+++ grails-app/init/tempvs/Application.groovy
@@ -3,9 +3,11 @@ package tempvs
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestTemplate
 
+@EnableDiscoveryClient
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
