45f69928c4c184be0c14fc38aa35de2196a66711
Anton Hlinisty <anton.hlinisty@gmail.com>
2019-10-27 16:07:46 +0300

    Cleanse tests

--- src/test/groovy/club/tempvs/rest/RestCallerSpec.groovy
+++ src/test/groovy/club/tempvs/rest/RestCallerSpec.groovy
@@ -1,8 +1,5 @@
 package club.tempvs.rest
 
-import club.tempvs.user.Profile
-import club.tempvs.user.User
-import club.tempvs.user.UserService
 import grails.converters.JSON
 import grails.test.mixin.TestMixin
 import grails.test.mixin.web.ControllerUnitTestMixin
@@ -17,15 +14,9 @@ import spock.lang.Specification
 @TestMixin(ControllerUnitTestMixin)
 class RestCallerSpec extends Specification {
 
-    private static final Long LONG_ONE = 1L
-    private static final Long LONG_TWO = 2L
     private static final String URL = 'url'
-    private static final String TIMEZONE = 'Europe/Minsk'
 
     def json = Mock JSON
-    def user = Mock User
-    def userService = Mock UserService
-    def profile = Mock Profile
     def restTemplate = Mock RestTemplate
     def responseEntity = Mock ResponseEntity
     def httpHeaders = Mock HttpHeaders
@@ -41,18 +32,13 @@ class RestCallerSpec extends Specification {
     }
 
     void "Test call() for POST"() {
-        given:
-        String body = "body"
-        String role = "role"
-        List<String> roles = [role, role]
-
         when:
         def result = restCaller.call(URL, HttpMethod.POST, json)
 
         then:
         1 * restTemplate.exchange("http://${URL}", HttpMethod.POST, _ as HttpEntity, byte[].class) >> responseEntity
         1 * responseEntity.statusCode >> HttpStatus.OK
-        1 * responseEntity.body >> body
+        1 * responseEntity.body
         1 * responseEntity.headers >> httpHeaders
         0 * _
 
@@ -61,18 +47,13 @@ class RestCallerSpec extends Specification {
     }
 
     void "Test call() for GET"() {
-        given:
-        String body = "body"
-        String role = "role"
-        List<String> roles = [role, role]
-
         when:
         def result = restCaller.call(URL, HttpMethod.GET, json)
 
         then:
         1 * restTemplate.exchange("http://${URL}", HttpMethod.GET, _ as HttpEntity, byte[].class) >> responseEntity
         1 * responseEntity.statusCode >> HttpStatus.OK
-        1 * responseEntity.body >> body
+        1 * responseEntity.body
         1 * responseEntity.headers >> httpHeaders
         0 * _
 
